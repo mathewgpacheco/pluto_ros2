13787b1b05af3caf023b697ffdcecff8941de843
mathewgpacheco <mathewgpacheco@gmail.com>
2023-03-31 18:47:44 -0400

    my first pub/sub system

---
+++ pluto_controller/pluto_controller/my_publisher.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from pluto_interfaces.msg import TargetLight
+
+
+class MyPublisherNode(Node):
+
+    def __init__(self):
+        super().__init__("my_publisher")
+
+        #Message class, Name of topic, Queue message size
+        self.publisher_ = self.create_publisher(TargetLight, "topic",10)
+
+        # Change every 7 seconds
+        self.timer_ = self.create_timer(5, self.timer_callback)
+        self.i = 0
+    
+    def timer_callback(self):
+        msg = TargetLight()
+        msg.data= "This is my published message " + str(self.i)
+
+        
+        self.publisher_.publish(msg)
+        self.get_logger().info("Publishing a message")
+        self.i += 1
+
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = MyPublisherNode()
+
+    rclpy.spin(node)
+
+    #destroy node and shutdown ros2 communication
+    rclpy.shutdown()
---
+++ pluto_controller/pluto_controller/my_subscriber.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from pluto_interfaces.msg import TargetLight
+
+class MySubscriberNode(Node):
+
+
+    def __init__(self):
+        super().__init__("my_subscriber")
+        self.audio_subscriber_ = self.create_subscription(TargetLight,"topic",self.listener_callback,10)
+
+    def listener_callback(self,msg):
+        payload = msg.data
+        self.get_logger().info("I heard: " + payload)
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = MySubscriberNode()
+
+    #Spin means the node will keep running until killed
+    rclpy.spin(node)
+    
+    #destroy node and shutdown ros2 communication
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
