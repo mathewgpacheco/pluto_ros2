51876fc03b55394eb90ebd07fb02ddf07e1244ee
mathewgpacheco <mathewgpacheco@gmail.com>
2023-04-01 12:07:07 -0400

    robot movement

--- pluto_controller/pluto_controller/my_publisher.py
+++ pluto_controller/pluto_controller/my_publisher.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 import rclpy
+import keyboard
 from rclpy.node import Node
 from pluto_interfaces.msg import TargetLight
+from pluto_interfaces.msg import TargetMove
+from irobot_create_msgs.msg import WheelVels
+from irobot_create_msgs.msg import WheelTicks
+from irobot_create_msgs.msg import WheelStatus
 
 
 class MyPublisherNode(Node):
@@ -11,10 +16,41 @@ class MyPublisherNode(Node):
 
         #Message class, Name of topic, Queue message size
         self.publisher_ = self.create_publisher(TargetLight, "topic",10)
+
+        self.movement_publisher_ = self.create_publisher(TargetMove,"/target_move",10)
         # Change every 7 seconds
-        self.timer_ = self.create_timer(5, self.timer_callback)
-        self.i = 0
+        self.timer_ = self.create_timer(10, self.pub_move)
+        self.action = "forward"
+
     
+ 
+        
+    def pub_move(self):
+        msg = TargetMove()
+        header = self.get_clock().now().to_msg()
+        msg.data = self.action
+
+        msg.vel = WheelVels()
+        msg.tick = WheelTicks()
+        msg.status = WheelStatus()
+
+        msg.vel.header.stamp = header
+        msg.vel.velocity_left = 0.5
+        msg.vel.velocity_right = 0.5
+        
+        msg.tick.header.stamp = header
+        msg.tick.ticks_left = 1
+        msg.tick.ticks_right = 1
+
+        msg.status.header.stamp = header
+        msg.status.current_ma_left = 0
+        msg.status.current_ma_right =0
+        msg.status.pwm_left =0 
+        msg.status.pwm_right =0
+        msg.status.wheels_enabled = True
+        self.movement_publisher_.publish(msg)
+        self.get_logger().info("sending a command now " + str(msg))
+
     def timer_callback(self):
         msg = TargetLight()
         msg.data= "This is my published message " + str(self.i)
--- pluto_controller/pluto_controller/my_subscriber.py
+++ pluto_controller/pluto_controller/my_subscriber.py
@@ -1,23 +1,52 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
+
+from irobot_create_msgs.msg import WheelVels
+from irobot_create_msgs.msg import WheelTicks
 from irobot_create_msgs.msg import WheelStatus
+
 from pluto_interfaces.msg import TargetLight
+from pluto_interfaces.msg import TargetMove
 
 class MySubscriberNode(Node):
 
 
     def __init__(self):
         super().__init__("my_subscriber")
-        self.my_subscriber_ = self.create_subscription(TargetLight,"topic",self.listener_callback,10)
-        self.wheel_status_subscriber_  = self.create_subscription(WheelStatus,"/wheel_status",self.status_callback, 10)
+        #self.my_subscriber_ = self.create_subscription(TargetLight,"topic",self.listener_callback,10)
 
-    def status_callback(self,msg):
-        self.get_logger().info("Status of wheels: " + str(msg))
+
+        #subscribe to movement commands
+        self.movement_subscriber_ = self.create_subscription(TargetMove, "/target_move", self.listener_callback,10)
+        self.get_logger().info("Move subscriber initialized")
+
+        #publish movement actions to robot
+        self.cmd_vel_pub = self.create_publisher(WheelVels,"/wheel_vels",10)
+        self.cmd_stat_pub = self.create_publisher(WheelTicks,"/wheel_ticks",10)
+        self.cmd_tick_pub = self.create_publisher(WheelStatus,"/wheel_status",10)
+
+
+    def send_movement_command(self,msg):
+        vel = WheelVels()
+        tick = WheelTicks()
+        status = WheelStatus()
+
+        #assign movement stuff here 
+        vel = msg.vel
+        #tick = msg.tick
+        #status = msg.status
+
+        self.cmd_vel_pub.publish(vel)
+        #self.cmd_tick_pub.publish(tick)
+        #self.cmd_stat_pub.publish(status)
+
+        self.get_logger().info("Moving... ")
 
     def listener_callback(self,msg):
         payload = msg.data
-        self.get_logger().info("I heard: " + payload)
+        self.get_logger().info("Message recieved...moving " + payload)
+        self.send_movement_command(msg)
 
 def main(args=None):
     rclpy.init(args=args)
